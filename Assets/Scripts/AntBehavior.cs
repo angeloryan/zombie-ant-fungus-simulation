545577375d4d3efd85462b2bb8040ef79ebfc7fd
angeloryan <sorianoangeloryan@gmail.com>
2021-12-05 00:02:08 -0800

    Unity Project

---
+++ Assets/Scripts/AntBehavior.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AntBehavior : MonoBehaviour
+{
+    Rigidbody rb;
+    Transform t;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        t = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (t.position.x > 10 || t.position.x < -10 || t.position.z > 10 || t.position.z < -10)
+        {
+            t.Rotate(new Vector3(0, t.rotation.y + 1, 0));
+        }
+
+        t.Translate(transform.forward * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/CameraMovement3D.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement3D : MonoBehaviour
+{
+    private bool wKeyPressed;
+    private bool aKeyPressed;
+    private bool sKeyPressed;
+    private bool dKeyPressed;
+    private bool spaceKeyPressed;
+    private bool shiftKeyPressed;
+
+    Transform t;
+
+    [SerializeField]
+    float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        t = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Checks for key down
+        if (Input.GetKeyDown(KeyCode.W)) 
+        {
+            wKeyPressed = true;
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            aKeyPressed = true;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            sKeyPressed = true;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            dKeyPressed = true;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            spaceKeyPressed = true;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            shiftKeyPressed = true;
+        }
+
+        // Checks for key up
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            wKeyPressed = false;
+        }
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            aKeyPressed = false;
+        }
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            sKeyPressed =false;
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            dKeyPressed = false;
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            spaceKeyPressed = false;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            shiftKeyPressed = false;
+        }
+
+    }
+
+    // Called once every fixed frame
+    private void FixedUpdate()
+    {   
+        // Checks to see if key is pressed
+        if (wKeyPressed && t.position.z < 25)
+        {
+            t.position = new Vector3(t.position.x, t.position.y, t.position.z + speed);
+        }
+
+        if (aKeyPressed && t.position.x > -25)
+        {
+            t.position = new Vector3(t.position.x - speed, t.position.y, t.position.z);
+        }
+
+        if (sKeyPressed && t.position.z > -25)
+        {
+            t.position = new Vector3(t.position.x, t.position.y, t.position.z - speed);
+        }
+
+        if (dKeyPressed && t.position.x < 25)
+        {
+            t.position = new Vector3(t.position.x + speed, t.position.y, t.position.z);
+        }
+
+        if (spaceKeyPressed && t.position.y < 25)
+        {
+            t.position = new Vector3(t.position.x, t.position.y + speed, t.position.z);
+        }
+
+        if (shiftKeyPressed && t.position.y > 0)
+        {
+            t.position = new Vector3(t.position.x, t.position.y - speed, t.position.z);
+        }
+    }
+}
